dd939c2ef3837ea98846f4dc375ab48710416907
Ima511 <abhaykumarjhaexam420@rediffmail.com>
2022-07-25 14:19:40 +0530

    Working of Battery APIs and landscape files added.

--- app/src/main/java/com/example/batteryhealth3/MainActivity.java
+++ app/src/main/java/com/example/batteryhealth3/MainActivity.java
@@ -15,17 +15,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         // step1
         setSupportActionBar(toolbar);
-
         // text as title
         getSupportActionBar().setTitle("Battery Health");
-
         //        toolbar.setTitle("Battery Fiction");
-
-
 
     }
 }
