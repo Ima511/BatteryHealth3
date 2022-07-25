2816fc7f854800b53554d6484d65df4043fbea6d
Ima511 <abhaykumarjhaexam420@rediffmail.com>
2022-07-25 18:45:27 +0530

    Battery Charge level API was Integrated.

--- app/src/main/java/com/example/batteryhealth3/MainActivity.java
+++ app/src/main/java/com/example/batteryhealth3/MainActivity.java
@@ -3,18 +3,44 @@ package com.example.batteryhealth3;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.nio.channels.InterruptedByTimeoutException;
 
 public class MainActivity extends AppCompatActivity {
 
     Toolbar toolbar;
-    TextView textView2;
+    TextView
+            tv_TimeLeftValue,
+            tv_AvailabilityValue,
+            tv_NominalVolValue,
+            tv_ConditionValue,
+            tv_tempValue,
+            tv_TechnologyName,
+            tv_cycleCountValue;
+    ImageView imageView;
+    MyReceiver myReceiver = new MyReceiver();
+    IntentFilter intentFilter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv_TimeLeftValue = (TextView) findViewById(R.id.tv_TimeLeftValue);
+        tv_AvailabilityValue = (TextView) findViewById(R.id.tv_AvailabilityValue);
+        tv_NominalVolValue = (TextView) findViewById(R.id.tv_NominalVolValue);
+        tv_ConditionValue = (TextView) findViewById(R.id.tv_ConditionValue);
+        tv_tempValue = (TextView) findViewById(R.id.tv_tempValue);
+        tv_TechnologyName = (TextView) findViewById(R.id.tv_TechnologyName);
+        tv_cycleCountValue = (TextView) findViewById(R.id.tv_cycleCountValue);
+
+        imageView = (ImageView) findViewById(R.id.imageView);
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         // step1
         setSupportActionBar(toolbar);
@@ -22,5 +48,30 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("Battery Health");
         //        toolbar.setTitle("Battery Fiction");
 
+        IntentFilter intentfilter = new IntentFilter();
+        intentfilter.addAction(Intent.ACTION_BATTERY_CHANGED);
+        this.registerReceiver(myReceiver, intentfilter);
+
     }
+
+
+
+
+
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//
+//        IntentFilter intentFilter = new IntentFilter();
+//        intentFilter.addAction(Intent.ACTION_BATTERY_CHANGED);
+//        this.registerReceiver(myReceiver, intentFilter);
+//
+//         }
+
+//    @Override
+//    protected void onStop() {
+//        super.onStop();
+//        this.unregisterReceiver(myReceiver);
+//
+//    }
 }
--- app/src/main/java/com/example/batteryhealth3/MyReceiver.java
+++ app/src/main/java/com/example/batteryhealth3/MyReceiver.java
@@ -1,15 +1,66 @@
 package com.example.batteryhealth3;
 
+import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.BatteryManager;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 public class MyReceiver extends BroadcastReceiver {
+
+    TextView
+            tv_TimeLeftValue,
+            tv_AvailabilityValue,
+            tv_NominalVolValue,
+            tv_ConditionValue,
+            tv_tempValue,
+            tv_TechnologyName,
+            tv_cycleCountValue;
+    ImageView imageView;
+
+    public MyReceiver() {
+    }
+
+
 
     @Override
     public void onReceive(Context context, Intent intent) {
         // TODO: This method is called when the BroadcastReceiver is receiving
         // an Intent broadcast.
-        throw new UnsupportedOperationException("Not yet implemented");
+        tv_TimeLeftValue = (TextView) ((Activity)context).findViewById(R.id.tv_TimeLeftValue);
+        
+
+        String actionString = intent.getAction();
+        Toast.makeText(context, actionString, Toast.LENGTH_SHORT).show();
+
+
+        // Are we charging / charged?
+        int status = intent.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
+        boolean isCharging = status == BatteryManager.BATTERY_STATUS_CHARGING ||
+                status == BatteryManager.BATTERY_STATUS_FULL;
+
+// How are we charging?
+        int chargePlug = intent.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1);
+        boolean usbCharge = chargePlug == BatteryManager.BATTERY_PLUGGED_USB;
+        boolean acCharge = chargePlug == BatteryManager.BATTERY_PLUGGED_AC;
+
+        // Display whatever the state in the form of a Toast
+        if(isCharging) {
+            Toast.makeText(context, "Battery is charging ", Toast.LENGTH_SHORT).show();
+
+        } else {
+            Toast.makeText(context, "Battery is not charging ", Toast.LENGTH_SHORT).show();
+        }
+
+        if (Intent.ACTION_BATTERY_CHANGED.equals(intent.getAction())){
+        tv_TimeLeftValue.setText(String.valueOf(intent.getIntExtra("level",0)) + "%");
+        }
+
     }
+
+
 }
